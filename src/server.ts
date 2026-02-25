61bb18d78dd0cbc3c11e3fe46e80663b89a4a2d4
Madson333 <madson333@hotmail.com>
2026-02-25 18:13:08 -0300

    Adds Docker configuration files

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ app.use('/api-docs', serve, setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => {
+app.listen(3333, '0.0.0.0', () => {
   console.log('Server is running on port 3333');
 });
